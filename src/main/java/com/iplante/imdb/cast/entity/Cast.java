aa015d795ff7da53aae264f414584683eedf8420
Isabelle Plante <iplantemn@gmail.com>
2020-08-26 13:57:42 -0500

    Create the `Cast` entity class

---
+++ src/main/java/com/iplante/imdb/cast/entity/Cast.java
@@ -0,0 +1,45 @@
+package com.iplante.imdb.cast.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Entity representing a cast member.
+ *
+ * @author Isabelle Plante
+ * @version 1
+ * @since 8/26/20
+ */
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cast {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date dateOfBirth;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String bio;
+}
