4db2107d21460494c992380cab7895db6dd09b04
Isabelle Plante <iplantemn@gmail.com>
2020-08-27 18:56:39 -0500

    Add a new Repository method/endpoint to find all cast with IDs in a given list (will be used by the Movies API)

--- src/main/java/com/iplante/imdb/cast/repository/CastRepository.java
+++ src/main/java/com/iplante/imdb/cast/repository/CastRepository.java
@@ -1,9 +1,12 @@
 package com.iplante.imdb.cast.repository;
 
 import com.iplante.imdb.cast.entity.Cast;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
@@ -19,18 +22,32 @@ import java.util.List;
 public interface CastRepository extends PagingAndSortingRepository<Cast, Long> {
 
     /**
+     * Find all cast with IDs in a given list.
+     *
+     * @param castIds  the list of cast IDs.
+     * @param pageable the pageable parameters.
+     * @return a list of cast.
+     */
+    @RestResource(path = "ids")
+    Page<Cast> findAllByIdIsIn(@Param("castIds") List<Long> castIds, Pageable pageable);
+
+    /**
      * Find all cast matching a given first name.
      *
      * @param firstName the first name to find.
+     * @param pageable the pageable parameters.
      * @return a list of cast matching the given first name.
      */
-    List<Cast> findAllByFirstNameEquals(@Param("firstName") String firstName);
+    @RestResource(path = "firstName")
+    Page<Cast> findAllByFirstNameEquals(@Param("firstName") String firstName, Pageable pageable);
 
     /**
      * Find all cast matching a given last name.
      *
      * @param lastName the last name to find.
+     * @param pageable the pageable parameters.
      * @return a list of cast matching the given last name.
      */
-    List<Cast> findAllByLastNameEquals(@Param("lastName") String lastName);
+    @RestResource(path = "lastName")
+    Page<Cast> findAllByLastNameEquals(@Param("lastName") String lastName, Pageable pageable);
 }
