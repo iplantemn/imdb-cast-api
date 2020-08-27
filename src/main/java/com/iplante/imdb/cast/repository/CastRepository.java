40f0913d8dca909022d3edaa62cf7facd3dcae3a
Isabelle Plante <iplantemn@gmail.com>
2020-08-27 12:12:31 -0500

    Add search by first and last name endpoints to `CastRepository`

--- src/main/java/com/iplante/imdb/cast/repository/CastRepository.java
+++ src/main/java/com/iplante/imdb/cast/repository/CastRepository.java
@@ -2,7 +2,10 @@ package com.iplante.imdb.cast.repository;
 
 import com.iplante.imdb.cast.entity.Cast;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 /**
  * Repository for {@link Cast} entities, additionally exposing CRUD operations as pageable and
@@ -14,4 +17,20 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "cast", path = "cast")
 public interface CastRepository extends PagingAndSortingRepository<Cast, Long> {
+
+    /**
+     * Find all cast matching a given first name.
+     *
+     * @param firstName the first name to find.
+     * @return a list of cast matching the given first name.
+     */
+    List<Cast> findAllByFirstNameEquals(@Param("firstName") String firstName);
+
+    /**
+     * Find all cast matching a given last name.
+     *
+     * @param lastName the last name to find.
+     * @return a list of cast matching the given last name.
+     */
+    List<Cast> findAllByLastNameEquals(@Param("lastName") String lastName);
 }
